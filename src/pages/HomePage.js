81f468d569251f569e78647c6ba7e85e810db180
thai hien <phanthaihien000@gmail.com>
2024-05-31 15:17:04 +0700

    update 31/5/2024

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Footer from "../components/Footer";
+
+function HomePage() {
+  return (
+    <>
+      <p>aaa</p>
+      <Footer />
+    </>
+  );
+}
+
+export default HomePage;
---
+++ src/routes/PrivateRoute.js
@@ -0,0 +1,8 @@
+// import React from "react";
+// import { Route, Redirect, useLocation } from "react-router-dom";
+// const PrivateRoute = ({ children, ...rest }) => {
+//   if (rest.path === "/") {
+//     <Redirect to={location.state?.from ?? "/"} />;
+//   }
+// };
+// export default PrivateRoute;
